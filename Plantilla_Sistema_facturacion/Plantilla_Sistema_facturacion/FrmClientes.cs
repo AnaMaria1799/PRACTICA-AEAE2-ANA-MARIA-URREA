f02635f7f45c692365b08d701fb58a9d01cb4cb3
AnaMaria1799 <anamaria17123@gmail.com>
2025-03-06 11:32:24 -0500

    muchos cambios

--- Plantilla_Sistema_facturacion/Plantilla_Sistema_facturacion/FrmClientes.cs
+++ Plantilla_Sistema_facturacion/Plantilla_Sistema_facturacion/FrmClientes.cs
@@ -17,6 +17,37 @@ namespace Plantilla_Sistema_facturacion
             InitializeComponent();
         }
 
+        public int IdCliente { get; set; }
+
+        DataTable dt = new DataTable();
+        Acceso_datos Acceso =  new Acceso_datos();
+
+        private void    LLENAR_CLIENTE()
+        {
+            if (IdCliente == 0)
+            {
+                lblTitulo.Text = "INGRESO NUEVO CLIENTE";
+            }
+            else
+            {
+                string sentencia = $"select = from TBLCLIENTES where IdCliente = {IdCliente}";
+
+                dt = Acceso.EjecutarComandoDatos(sentencia);
+                foreach (DataRow row in dt.Rows)
+                {
+                    txtNombre.Text = row[1].ToString();
+                    txtDocumento.Text = row[2].ToString();
+                    txtDireccion.Text = row[3].ToString();
+                    txtTelefono.Text = row[4].ToString();
+                    txtEmail.Text = row[5].ToString();
+                }
+            }
+        }
+
+        private void frmClientes_Load(object sender, EventArgs e)
+        {
+            LLENAR_CLIENTE();
+        }
         private void btnGuardar_Click(object sender, EventArgs e)
         {
             if (txtNombre.Text == "")
