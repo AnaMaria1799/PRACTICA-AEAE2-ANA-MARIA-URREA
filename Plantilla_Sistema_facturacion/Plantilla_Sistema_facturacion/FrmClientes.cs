d5aa9c302b191bbc36d4902bcdc35b6727d52a3d
AnaMaria1799 <anamaria17123@gmail.com>
2025-03-06 11:55:53 -0500

    muchos cambios

--- Plantilla_Sistema_facturacion/Plantilla_Sistema_facturacion/FrmClientes.cs
+++ Plantilla_Sistema_facturacion/Plantilla_Sistema_facturacion/FrmClientes.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialSkin.Controls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,7 +11,7 @@ using System.Windows.Forms;
 
 namespace Plantilla_Sistema_facturacion
 {
-    public partial class FrmClientes : Form
+    public partial class FrmClientes : MaterialForm
     {
         public FrmClientes()
         {
@@ -26,7 +27,7 @@ namespace Plantilla_Sistema_facturacion
         {
             if (IdCliente == 0)
             {
-                lblTitulo.Text = "INGRESO NUEVO CLIENTE";
+                lblDatosCliente.Text = "INGRESO NUEVO CLIENTE";
             }
             else
             {
@@ -48,6 +49,69 @@ namespace Plantilla_Sistema_facturacion
         {
             LLENAR_CLIENTE();
         }
+
+        // ACTUALIZACIONES
+
+        //Funciones que permiten el ingreso, retiro y actualizaicon de clientes en la base de datos
+        public bool Guardar()
+        {
+            Boolean actualizado = false;
+            if (validar())
+            {
+                try
+                {
+                    Acceso_datos Acceso = new Acceso_datos();
+                    string sentencia = $"Exec [actualizar_Cliente] {IdCliente},'{txtNombre.Text}',{TxtDocumento.Text} ,'{txtDireccion.Text}','{txtTelefono.Text}', '{txtEmail.Text}'
+                   ,'Javier','{DateTime.Now.ToShortDateString()}'";
+                MessageBox.Show(Acceso.EjecutarComando(sentencia));
+                    actualizado = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("falló inserción: " + ex);
+                    actualizado = false;
+                }
+            }
+            return actualizado;
+        }
+
+        // funcion que permite validar los campos del formulario
+        private Boolean validar()
+        {
+            Boolean errorCampos = true;
+            if (txtNombre.Text == string.Empty)
+            {
+                campoVacioErrorProvider.SetError(txtNombre, "debeingresar el nombre del Cliente");
+                txtNombre.Focus();
+                errorCampos = false;
+            }
+            else { campoVacioErrorProvider.SetError(txtNombre, ""); }
+            if (txtDocumento.Text == "")
+            {
+                campoVacioErrorProvider.SetError(txtDocumento, "debe ingresar el documento");
+                campoVacioErrorProvider.Focus();
+                errorCampos = false;
+            }
+            else { campoVacioErrorProvider.SetError(txtDocumento, ""); }
+
+            if (!esNumerico(txtDocumento.Text))
+            {
+                campoVacioErrorProvider.SetError(txtDocumento, "El Documento debe ser numerico");
+                txtDocumento.Focus();
+                return false;
+            }
+            campoVacioErrorProvider.SetError(txtDocumento, "");
+                return errorCampos;
+        }
+
+
+
+
+
+
+
+
+
         private void btnGuardar_Click(object sender, EventArgs e)
         {
             if (txtNombre.Text == "")
